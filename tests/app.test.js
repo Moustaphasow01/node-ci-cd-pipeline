eb030615bd21375279ec27de143b58862c7065b9
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2025-02-07 00:17:43 +0100

    inclure Jest

---
+++ tests/app.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest");
+const app = require("../server");
+
+describe("GET /", () => {
+    it("should return a message", async () => {
+        const res = await request(app).get("/");
+        expect(res.status).toBe(200);
+        expect(res.body.message).toBe("Hello, world!");
+    });
+});
+
